e6175af656b67701d9cfa569daccacd97aebf6f5
levimarkmuigai <levimarkmuigai@gmail.com>
2025-05-17 17:52:51 +0300

    AddedCRUDOpertionsOnClinicEntity

--- src/main/java/com/example/MamaCare/controller/ClinicController.java
+++ src/main/java/com/example/MamaCare/controller/ClinicController.java
@@ -4,10 +4,49 @@ import com.example.MamaCare.model.Clinic;
 
 import com.example.MamaCare.repositories.ClinicRepository;
 
+import java.util.List;
+
+import java.util.Optional;
+
 public class ClinicController{
    private final ClinicRepository operation;
 
    public ClinicController(ClinicRepository operation){
         this.operation = operation;
+   }
+
+   // Create a Clinic entity
+   public Clinic createClinic(Clinic clinic){
+        return operation.save(clinic);
+   }
+
+   // List all Clinic's
+   public List<Clinic> fetchClinicList(){
+        return operation.findAll();
+   }
+
+   //Find a single Clinic by id
+   public Optional<Clinic> fetchClinicById(Long id){
+       return operation.findById(id);
+    }
+
+   //Update existing Clinic entity by id
+   public Optional<Clinic> updateClinic(Clinic updateClinic, Long id){
+        return operation.findById(id).map(clinic -> {
+            clinic.setName(updateClinic.getName());
+            clinic.setJoinedAt(updateClinic.getJoinedAt());
+            clinic.setLocation(updateClinic.getLocation());
+
+            Clinic saveClinic = operation.save(clinic);
+
+            return saveClinic;
+        });
+   }
+
+   // Delete Clinic entity by id
+   public void deleteClinicById(Long id){
+        if(operation.existsById(id)){
+            operation.deleteById(id);
+        }
    }
 }
